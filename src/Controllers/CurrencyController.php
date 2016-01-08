5c9fa9650024f40cc2d09b8e258d688c6ed54114
Jean-Guy Landriault <jean-guy@landriault.com>
2016-01-08 09:21:27 -0500

    Removed scope test

--- src/Controllers/CurrencyController.php
+++ src/Controllers/CurrencyController.php
@@ -26,7 +26,7 @@ class CurrencyController extends Controller
 			return view( config('lex.views.unauthorized'), [ 'message' => 'view currency list' ]);
 		}
 
-		$currencies = Currency::convertible()->paginate( config('lex.pagination', 15) );
+		$currencies = Currency::paginate( config('lex.pagination', 15) );
 		return view( config('lex.views.index'), compact('currencies') );
 		
 	}
