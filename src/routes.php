af5b71e31151c1e530a2edd7a01e7c862fa8c965
landjea <jean-guy@landriault.com>
2016-01-15 13:56:00 -0500

    Update routes for 5.2

--- src/routes.php
+++ src/routes.php
@@ -1,3 +1,9 @@
 <?php
 
-Route::resource('lex', 'Smarch\Lex\Controllers\CurrencyController');
+if ( str_contains( app()->version(), '5.2.' ) ){
+	Route::group(['middleware' => 'web'], function () {
+		Route::resource('lex', 'Smarch\Lex\Controllers\CurrencyController');
+	});
+} else {
+	Route::resource('lex', 'Smarch\Lex\Controllers\CurrencyController');
+}
