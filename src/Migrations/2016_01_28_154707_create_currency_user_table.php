33280ac2e7d4ae60bc0356167b684f9be63ec53d
landjea <jean-guy@landriault.com>
2016-01-28 10:55:56 -0500

    Migration for currency_user table. Checks for characters table.

---
+++ src/Migrations/2016_01_28_154707_create_currency_user_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCurrencyUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('currency_user', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned()->index();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            if (Schema::hasTable('characters')) {
+                $table->integer('character_id')->unsigned()->index();
+            }
+            $table->integer('currency_id')->unsigned()->index();
+            $table->integer('currency_quantity')->unsigned()->index();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('currency_user');
+    }
+}
