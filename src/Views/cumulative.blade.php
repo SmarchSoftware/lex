5b4badebe9ed80ec20e3a29a5c1b235ea7a68bf9
landjea <jean-guy@landriault.com>
2016-02-28 10:43:02 -0500

    Added a Select All box

--- src/Views/cumulative.blade.php
+++ src/Views/cumulative.blade.php
@@ -72,24 +72,32 @@
           {!! Form::model($resource, [ 'route' => [ config('lex.route.as') . 'cumulative', $resource->id ], 'class' => 'form-horizontal']) !!}
                     
           <fieldset>
-          <legend>Assign
-          <input type="number" placeholder="1" value=1 name="quantity"> <strong>{{ str_plural($resource->name) }}</strong> to each of the checked characters below. <br />
-          <small class="text-muted">Positive amounts will be added, negative amounts will be removed</small>
-          </legend>
-
-          <div style="overflow:auto; max-height:250px">
-          @forelse($characters->chunk(6) as $c)
-            @foreach ($c as $u)
-            <div class="col-md-2 col-sm-3 col-xs-6">
-            <label class="checkbox-inline" title="{{ $u->name }}">
-              <input type="checkbox" name="character_id[]" value="{{$u->id}}"> {{ $u->name }}
-            </label>
+            <legend>Assign
+              <input type="number" placeholder="1" value=1 name="quantity"> <strong>{{ str_plural($resource->name) }}</strong> to each of the checked characters below. <br />
+              <small class="text-muted">Positive amounts will be added, negative amounts will be removed</small>
+            </legend>
+            
+            <div class="row">
+              <div class="col-md-12 col-sm-12 col-xs-12">
+                <input type="checkbox" name="select_all" id="select_all" onChange="$('.checkbox_class').prop('checked', $(this).prop('checked'));"> Select All
+              </div>
             </div>
-            @endforeach
-          @empty
-            <span class="text-warning"><i class="fa fa-warning text-warning"></i> There aren't any characters....?</span>
-          @endforelse
-          </div>
+
+            <div class="row">
+              <div class="col-md-12 col-sm-12 col-xs-12" style="overflow:auto; max-height:250px">
+                @forelse($characters->chunk(6) as $c)
+                  @foreach ($c as $u)
+                  <div class="col-md-2 col-sm-3 col-xs-6">
+                  <label class="checkbox-inline" title="{{ $u->name }}">
+                    <input type="checkbox" class="checkbox_class" name="character_id[]" value="{{$u->id}}"> {{ $u->name }}
+                  </label>
+                  </div>
+                  @endforeach
+                @empty
+                  <span class="text-warning"><i class="fa fa-warning text-warning"></i> There aren't any characters....?</span>
+                @endforelse
+              </div>
+            </div>
 
           </fieldset>
 
